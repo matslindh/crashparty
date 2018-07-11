b91fa92b0cbffb761ccdfd3ad964466c5ea04953
Mats Lindh <mats@lindh.no>
2018-07-11 22:31:06 +0200

    Add dependency on WMI

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='crashparty',
     version='0.0.1',
-    packages=['crashparty'],
     include_package_data=True,
     install_requires=[
         'flask',
         'flask_sse',
         'memorpy',
+        'WMI',
     ],
 )
